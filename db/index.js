d6d8ef0d917bbff5a7aab9bac569b4806b4549b8
Isaac <isaachernandez8@gmail.com>
2022-10-01 14:55:56 -0700

    finished part 3 of part 2

--- db/index.js
+++ db/index.js
@@ -1,10 +1,6 @@
-const { Client } = require('pg') // imports the pg module
+const { Client } = require('pg')
 
 const client = new Client('postgres://localhost:5432/juicebox-dev');
-
-/**
- * USER Methods
- */
 
 async function createUser({ 
   username, 
@@ -27,12 +23,10 @@ async function createUser({
 }
 
 async function updateUser(id, fields = {}) {
-  // build the set string
   const setString = Object.keys(fields).map(
     (key, index) => `"${ key }"=$${ index + 1 }`
   ).join(', ');
 
-  // return early if this is called without fields
   if (setString.length === 0) {
     return;
   }
@@ -84,10 +78,6 @@ async function getUserById(userId) {
   }
 }
 
-/**
- * POST Methods
- */
-
 async function createPost({
   authorId,
   title,
@@ -110,17 +100,14 @@ async function createPost({
 }
 
 async function updatePost(postId, fields = {}) {
-  // read off the tags & remove that field 
-  const { tags } = fields; // might be undefined
+  const { tags } = fields;
   delete fields.tags;
 
-  // build the set string
   const setString = Object.keys(fields).map(
     (key, index) => `"${ key }"=$${ index + 1 }`
   ).join(', ');
 
   try {
-    // update any fields that need to be updated
     if (setString.length > 0) {
       await client.query(`
         UPDATE posts
@@ -130,18 +117,15 @@ async function updatePost(postId, fields = {}) {
       `, Object.values(fields));
     }
 
-    // return early if there's no tags to update
     if (tags === undefined) {
       return await getPostById(postId);
     }
 
-    // make any new tags that need to be made
     const tagList = await createTags(tags);
     const tagListIdString = tagList.map(
       tag => `${ tag.id }`
     ).join(', ');
 
-    // delete any post_tags from the database which aren't in that tagList
     await client.query(`
       DELETE FROM post_tags
       WHERE "tagId"
@@ -149,7 +133,6 @@ async function updatePost(postId, fields = {}) {
       AND "postId"=$1;
     `, [postId]);
 
-    // and create post_tags as necessary
     await addTagsToPost(postId, tagList);
 
     return await getPostById(postId);
@@ -182,6 +165,13 @@ async function getPostById(postId) {
       FROM posts
       WHERE id=$1;
     `, [postId]);
+
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId"
+      };
+    }
 
     const { rows: tags } = await client.query(`
       SELECT tags.*
@@ -243,10 +233,6 @@ async function getPostsByTagName(tagName) {
   }
 } 
 
-/**
- * TAG Methods
- */
-
 async function createTags(tagList) {
   if (tagList.length === 0) {
     return;
@@ -261,14 +247,12 @@ async function createTags(tagList) {
   ).join(', ');
 
   try {
-    // insert all, ignoring duplicates
     await client.query(`
       INSERT INTO tags(name)
       VALUES (${ valuesStringInsert })
       ON CONFLICT (name) DO NOTHING;
     `, tagList);
 
-    // grab all and return
     const { rows } = await client.query(`
       SELECT * FROM tags
       WHERE name
@@ -349,5 +333,6 @@ module.exports = {
   getAllTags,
   createPostTag,
   addTagsToPost,
-  getUserByUsername
+  getUserByUsername,
+  getPostById
 }
